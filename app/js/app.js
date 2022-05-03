3750c0ff20ab712b8b3236d9400fb5b91bd01774
Beshiimov <beshiimov2001@gmail.com>
2022-05-03 06:59:05 +0300

    v 1.2

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
+
+const featuredWorkDown = document.querySelector('.scrollDown').addEventListener('click', () => {
+  console.log(`lll`)
+});
