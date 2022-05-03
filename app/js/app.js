af0e381311ee7222b2999576a10f8036b0325a98
Beshiimov <93453697+Beshiimov@users.noreply.github.com>
2022-05-03 07:27:45 +0300

    Delete app directory

--- app/js/app.js
+++
@@ -1,6 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-flsFunctions.isWebp();
-
-const featuredWorkDown = document.querySelector('.scrollDown').addEventListener('click', () => {
-  console.log(`lll`)
-});
